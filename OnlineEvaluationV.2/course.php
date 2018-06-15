bcdefa3c779ec0ee96dae56593c2d212ca839e57
VictoriaBendiii <2160316@slu.edu.ph>
2018-06-15 14:52:37 +0800

    Modified course.php and course_style.css

--- OnlineEvaluationV.2/course.php
+++ OnlineEvaluationV.2/course.php
@@ -3,13 +3,16 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta charset="UTF-8"/>
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SLU Peer Evaluation | Home</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" href="css/images/slogo.png">
     <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/course_style.css">
 </head>
 
@@ -17,74 +20,82 @@
     <div class="wrapper">
         <header>
             <nav style="z-index: 1000;">
-                    <div class="menu-icon">
-                        <i class="fa fa-bars fa-2x"></i>
-                    </div>
-					<img src="css/images/slogo.png" style="height: 45px; width: 36px; position: fixed; top: 10px; left: 10px;">
-                    <div class="logo">&emsp;SLU Peer Evaluation</div>
-                    <div class="menu">
-                        <ul>
-                            <li><a href="homepageStudent.php">Home</a></li>
-                            <li><a class="active" href="classes.php">Classes</a></li>
-							<li><a href="about.html">About</a></li>
-                            <li style="color: white; font-size: 20px; ">Welcome &nbsp; <a href="profile.php"> 
-                            <?php echo $_SESSION['firstname']. " ". $_SESSION['lastname'];?> </a>
+                <div class="menu-icon">
+                    <i class="fa fa-bars fa-2x"></i>
+                </div>
+                <img src="css/images/slogo.png" style="height: 45px; width: 36px; position: fixed; top: 10px; left: 10px;">
+                <div class="logo">&emsp;SLU Peer Evaluation</div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="homepageStudent.php">Home</a></li>
+                        <li><a class="active" href="classes.php">Classes</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li style="color: white; font-size: 20px; ">Welcome &nbsp;
+                            <a href="profile.php">
+                                <?php echo $_SESSION['firstname']. " ". $_SESSION['lastname'];?> </a>
                         </li>
 
-                        </ul>
-                    </div>
-                </nav>
+                    </ul>
+                </div>
+            </nav>
         </header>
-    
-                <div class="header">
-                    <h1> <?php echo "<h1>".$_SESSION['courseCode']. "</h1> <br> <h2>". $_SESSION['courseName']."</h2>";?> </h1>
-                </div>
-                <div class="left">
-                    <h1><center> Activities </center></h1> <hr>
-                    <p>Left Menu</p>
-                </div>
+    </div>
 
-                <div class="main">
-                    <p>Main Content</p>
-                </div>
-        
-        
+    <div class="container">
+        <div class="head">
+            <center>
+                <h1>
+                    <?php echo "<h1>".$_SESSION['courseCode']. "</h1> <br> <h2>". $_SESSION['courseName']."</h2>";?></h1>
+                <p>Course Name and Teacher's Name will be shown above</p>
+            </center>
+        </div>
+        <div class="row">
+            <div class="col-6 col-md-4" id="left">
+                <h2>Activities </h2>
+                <p> Activities will be shown here and calendar will be shown below <br> etcetera <br> etcetera etcetera</p>
+
+            </div>
+            <div class="col-12 col-md-8">
+                <h2>Posts</h2>
+                <p> This is where the posts go </p>
+            </div>
+        </div>
+
         <!-- script for nav bar -->
         <script type="text/javascript">
-        
-        $(function() {
-           var welcomeSection = $('.welcome-section'),
-               enterButton = welcomeSection.find('.enter-button');
-            
-            setTimeout(function() {
-                welcomeSection.removeClass('content-hidden');
-            },800);
-            
-            enterButton.on('click', function(e){
-               e.preventDefault();
-                welcomeSection.addClass('content-hidden').fadeOut();
+            $(function() {
+                var welcomeSection = $('.welcome-section'),
+                    enterButton = welcomeSection.find('.enter-button');
+
+                setTimeout(function() {
+                    welcomeSection.removeClass('content-hidden');
+                }, 800);
+
+                enterButton.on('click', function(e) {
+                    e.preventDefault();
+                    welcomeSection.addClass('content-hidden').fadeOut();
+                });
             });
-        });
-            
+
         </script>
-        
+
         <script type="text/javascript">
-        
-        $(document).ready(function(){
-            $(".menu-icon").on("click", function(){
-                $("nav ul").toggleClass("showing");
+            $(document).ready(function() {
+                $(".menu-icon").on("click", function() {
+                    $("nav ul").toggleClass("showing");
+                });
             });
-        });
-            
-        $(window).on("scroll", function(){
-            if($(window).scrollTop()) {
-                $('nav').addClass('black');
-            } else {
-                $('nav').removeClass('black');
-            }
-        })    
-            
+
+            $(window).on("scroll", function() {
+                if ($(window).scrollTop()) {
+                    $('nav').addClass('black');
+                } else {
+                    $('nav').removeClass('black');
+                }
+            })
+
         </script>
     </div>
-    </body>
+</body>
+
 </html>

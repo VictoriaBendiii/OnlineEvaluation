91d9e0aeca3fbf8956e65f41400b3de7b4764eae
2166244 <35590223+2166244@users.noreply.github.com>
2018-06-15 09:46:26 +0800

    Added file for homepage of student and join class

--- OnlineEvaluation/classes.php
+++ OnlineEvaluation/classes.php
@@ -6,9 +6,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>SLU Peer Evaluation | Home</title>
-        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="css/styles.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="icon" href="css/images/slogo.png">
+        	<link rel="favicon" href="assets/images/favicon.png">
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css"> 
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen"> 
+	<link rel="stylesheet" href="assets/css/style.css">
+    <link rel='stylesheet' id='camera-css'  href='assets/css/camera.css' type='text/css' media='all'>
         <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
     </head>
     
@@ -24,23 +31,117 @@
                     <div class="logo">&emsp;SLU Peer Evaluation</div>
                     <div class="menu">
                         <ul>
-                            <li><a  href="homepageStudent.php">Home</a></li>
-                            <li><a class="active" href="classes.php">Classes</a></li>
-							<li><a href="about.html">About</a></li>
-                            <li style="color: white; font-size: 20px; ">Welcome &nbsp; <a href="profile.php"> 
+                            <li><a class="active" href="teacherpage.php">Home</a></li>
+                            <li><a href="#myModal">Join A Class</a></li>
+                            <li style="color: white; font-size: 20px; "><a href="profile.php"> Welcome &nbsp; 
                             <?php echo $_SESSION['firstname']. " ". $_SESSION['lastname'];?> </a>
                         </li>
+                            <li><a href="signout.php">Log-out</a></li>
 
                         </ul>
                     </div>
                 </nav>
-                            <p class="line anim-typewriter">"Online Peer Evaluation for Louisians"</p>
-            </header>
+                 <div class="container">
+    <div class="row">
+					<?php
+                    $target_name = array();
+                $user = mysqli_real_escape_string($conn, $_SESSION['username']);
+                $target = "classes.php";
+                $sql = "select * from user_course join users using(id) join course using(courseCode) where users.username ='$user'";
+                $result = mysqli_query($conn, $sql);
 
+                    if (mysqli_num_rows($result) > 0 or mysqli_num_rows($result) == 0) {
+                        // output data of each row
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<div class='col-md-3'> <div class='grey-box-icon'> <div class='icon-box-top grey-box-icon-pos'>
+                                        <img src='assets/images/1.PNG' alt='' style='width: 100px; height: auto;' />
+                                    </div><a href='classes.php'> <h4>&nbsp; <div> ".$row["courseCode"].  "<br>" . $row["courseName"]." <br></h4> </a>
+						</div>
+					</div>";
+                        
+                        }
+                         echo "<a><div class='col-md-2' data-toggle='modal' data-target='#myModal'></a>
+                                <div class='grey-box-icon'> 
+                                    <div class='icon-box-top grey-box-icon-pos' style='margin: 0px;'>
+                                        <img src='assets/images/2.PNG' alt='' style='width: 100px; height: auto;' />
+                                    </div> 
+                                <h4 style='margin: 10px; padding: 0px; color: black;'>Add a class</h4>
+						      </div>
+					       </div></a>";
+                    
+                    } else{
+                            echo "<a><div class='col-md-2' data-toggle='modal' data-target='#myModal'></a>
+                                <div class='grey-box-icon'> 
+                                    <div class='icon-box-top grey-box-icon-pos' style='margin: 0px;'>
+                                        <img src='assets/images/2.PNG' alt='' style='width: 100px; height: auto;' />
+                                    </div> 
+                                <h4 style='margin: 10px; padding: 0px; color: black;'>Add a class</h4>
+						      </div>
+					       </div></a>";
+                        }
+        
+        if(isset($_POST['join'])){
             
-             <div class="contents"> 
+            $query = "SELECT * FROM USER_COURSE JOIN COURSE USING (courseCode) JOIN users using (id) where identification= 'teacher' and username='".$_SESSION["username"]."'";
+            $result = mysqli_query($conn,$query);
+            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);     
+            $_SESSION['id'] = $row['id'];
+            $id = $_SESSION['id'];
+			
+			$query2 = "INSERT INTO user_course (id, courseCode, groupID) VALUES ('$id', '".$_POST["code"]."',null)";
+			
             
-            </div>
+			if ($conn->multi_query($query2) === TRUE) {
+                    echo "<div class='col-md-3'> <div class='grey-box-icon'> <div class='icon-box-top grey-box-icon-pos'>
+                                        <img src='assets/images/1.PNG' alt='' style='width: 100px; height: auto;' />
+                                    </div><a href='classes.php'> <h4>&nbsp; <div> ".$row["courseCode"].  "<br>" . $row["courseName"]." <br></h4> </a>
+						</div>
+					</div>";
+}
+        }   $conn->close();
+        
+    ?>
+  
+      </div>
+    
+    </div>
+            </header>
+                          <!-- Modal -->
+      <form id ="classForm" method = "post" class="form-horizontal">
+      
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" >
+                      <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header" style="background-color: #3d84e6">
+                            <h5 class="modal-title" id="exampleModalLongTitle" style="text-align: center; color: white; font-size: 2em">JOIN A CLASS</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body" style="text-align: center; font-size: 1.3em;">
+                            To join a class, type in the code given <br> by your teacher below
+                              <div class="form-group">
+                                  <label for="usr"></label>
+                                    <input type="text" class="form-control" id="usr" placeholder="code" name="classcode">
+                              </div>               
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" name ="join">Join class</button>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+      </form>  
+            	<script src="assets/js/modernizr-latest.js"></script> 
+	<script type='text/javascript' src='assets/js/jquery.min.js'></script>
+    <script type='text/javascript' src='assets/js/fancybox/jquery.fancybox.pack.js'></script>
+    
+    <script type='text/javascript' src='assets/js/jquery.mobile.customized.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery.easing.1.3.js'></script> 
+    <script type='text/javascript' src='assets/js/camera.min.js'></script> 
+    <script src="assets/js/bootstrap.min.js"></script> 
+	<script src="assets/js/custom.js"></script>
             
             <script type="text/javascript">
         
